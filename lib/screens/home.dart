2d0080006969901d645b939d5a5d6acbad387b04
nhat2012782 <gonbbb1@gmail.com>
2024-01-28 23:26:24 +0700

    Add searchBox, label All Todos

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:todo_app/constants/colors.dart';
 
@@ -7,11 +8,42 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: tdBGColor,
-      appBar: _buildAppBar(),
-      body: Container(
-        child: Text('This is home screen.'),
-      ),
+        backgroundColor: tdBGColor,
+        appBar: _buildAppBar(),
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Column(children: [
+            searchBox(),
+            Expanded(
+                child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 50, bottom: 20),
+                  child: Text(
+                    'All ToDos',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                  ),
+                )
+              ],
+            ))
+          ]),
+        ));
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+          decoration: const InputDecoration(
+              contentPadding: EdgeInsets.all(0),
+              prefixIcon: Icon(Icons.search, color: tdBlack, size: 20),
+              prefixIconConstraints:
+                  BoxConstraints(maxHeight: 20, minWidth: 25),
+              border: InputBorder.none,
+              hintText: 'Search',
+              hintStyle: TextStyle(color: tdGrey))),
     );
   }
 
