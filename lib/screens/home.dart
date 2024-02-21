d9ca8d780485277a067da5f999e1e7bfcd85a75a
nhat2012782 <gonbbb1@gmail.com>
2024-02-21 22:59:07 +0700

    Add radio option all, today, upcoming

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,10 +11,13 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+enum RadioOption { all, today, upcoming }
+
 class _HomeState extends State<Home> {
   final todosList = ToDo.todoList();
   List<ToDo> _foundToDo = [];
   final _todoController = TextEditingController();
+  RadioOption? _radioValue = RadioOption.all;
 
   @override
   void initState() {
@@ -34,11 +37,12 @@ class _HomeState extends State<Home> {
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
               child: Column(children: [
                 searchBox(),
+                radioFilterBox(),
                 Expanded(
                     child: ListView(
                   children: [
                     Container(
-                      margin: EdgeInsets.only(top: 50, bottom: 20),
+                      margin: EdgeInsets.only(top: 0, bottom: 20),
                       child: Text(
                         'All ToDos',
                         style: TextStyle(
@@ -158,6 +162,77 @@ class _HomeState extends State<Home> {
     );
   }
 
+  Widget radioFilterBox() {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Row(
+                children: [
+                  Radio(
+                      value: RadioOption.all,
+                      groupValue: _radioValue,
+                      onChanged: (RadioOption? value) {
+                        setState(() {
+                          _radioValue = value;
+                        });
+                      }),
+                  Expanded(
+                      child: Text(
+                    'All',
+                    style: TextStyle(fontSize: 16),
+                  ))
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                children: [
+                  Radio(
+                      value: RadioOption.today,
+                      groupValue: _radioValue,
+                      onChanged: (RadioOption? value) {
+                        setState(() {
+                          _radioValue = value;
+                        });
+                      }),
+                  Expanded(
+                      child: Text(
+                    'Today',
+                    style: TextStyle(fontSize: 16),
+                  ))
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                children: [
+                  Radio(
+                      value: RadioOption.upcoming,
+                      groupValue: _radioValue,
+                      onChanged: (RadioOption? value) {
+                        setState(() {
+                          _radioValue = value;
+                        });
+                      }),
+                  Expanded(
+                    child: Text(
+                      'Upcoming',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+
   AppBar _buildAppBar() {
     return AppBar(
       backgroundColor: tdBGColor,
@@ -168,13 +243,13 @@ class _HomeState extends State<Home> {
           color: tdBlack,
           size: 30,
         ),
-        Container(
-          height: 40,
-          width: 40,
-          child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset('assets/images/avatar.png')),
-        )
+        // Container(
+        //   height: 40,
+        //   width: 40,
+        //   child: ClipRRect(
+        //       borderRadius: BorderRadius.circular(20),
+        //       child: Image.asset('assets/images/avatar.png')),
+        // )
       ]),
     );
   }
